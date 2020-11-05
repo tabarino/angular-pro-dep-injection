98e20b822ec71f2c417e70d72fa79648381e4cb3
tabarino <tabarino@outlook.com>
2020-11-05 23:31:13 +0000

    79 - NgZone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, NgZone, OnInit } from '@angular/core';
 import { FoodStoreService } from './food-store/food-store.service';
 
 @Component({
@@ -6,9 +6,28 @@ import { FoodStoreService } from './food-store/food-store.service';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck {
     title = 'angular-pro-dep-injection';
     store$ = this.foodService.getStore();
+    counter = 0;
 
-    constructor(private foodService: FoodStoreService) {}
+    constructor(
+        private foodService: FoodStoreService,
+        private zone: NgZone
+    ) {}
+
+    ngOnInit(): void {
+        this.zone.runOutsideAngular(() => {
+            for (let i = 0; i < 100; i++) {
+                setTimeout(() => this.counter++);
+            }
+            this.zone.run(() => {
+                setTimeout(() => this.counter = this.counter, 1000);
+            });
+        });
+    }
+
+    ngDoCheck(): void {
+        console.log('Change Detection has been run');
+    }
 }
